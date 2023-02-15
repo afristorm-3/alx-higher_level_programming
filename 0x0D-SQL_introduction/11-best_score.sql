3f2e8aee79a89ea2d30fbbf7c7bcef75bea418a5
afristorm-3 <afristorm3@gmail.com>
2023-02-14 19:49:31 -0500

    best score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
---lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `score` >= 10
-ORDER BY `score` DESC;
+-- Prints the sorted database, using WHERE filter keyword
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC, name;
