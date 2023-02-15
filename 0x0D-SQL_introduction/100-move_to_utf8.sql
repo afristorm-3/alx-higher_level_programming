3e765625e2996fb2c66b83569bcfdb4cdaaaa45e
afristorm-3 <afristorm3@gmail.com>
2023-02-14 20:02:21 -0500

    move to

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,3 +7,4 @@ ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Change database to utf-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
