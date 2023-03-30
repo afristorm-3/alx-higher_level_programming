d1958702b2ebd4407bf1417bd65db20885529bc3
afristorm-3 <afristorm3@gmail.com>
2023-03-30 17:21:04 -0400

    peak update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Module 6-peak
-Function to find the peak number
-"""
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """ Returns the peak number
-    Function to find the peak number
-    Args:
-        list_of_integers (list): The numners list.
-    """
-    li = list_of_integers
-    if li == []:
-        return (None)
-    if len(li) == 1:
-        return (li[0])
-    if li[0] > li[len(li) - 1]:
-        return (find_peak(li[:(len(li) + 1)//2]))
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        return (find_peak(li[(len(li))//2:]))
+        return find_peak(list_of_integers[mid + 1:])
