7783214a47ee865f063676f56b979cc7351e9ba6
afristorm-3 <afristorm3@gmail.com>
2023-03-30 17:05:16 -0400

    catchupdate

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#this script makes a request that causes the server to respond with a message
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin:Holbertonschool"
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
